8ca738a0112df6f29c17868d839289f28c0a5ac0
Marine Denolle <mdenolle@uw.edu>
2023-04-27 13:14:33 -0700

    update the cloud s3 feature extraction

--- feature_extraction_fromS3.py
+++ feature_extraction_fromS3.py
@@ -1,66 +1,91 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from datetime import date
 import pandas as pd
+import datetime
+import time
+import os
+import obspy
 from scipy.fftpack import fft, fftfreq, next_fast_len
-
-from obspy import read
+from obspy.clients.fdsn import Client
+# The S3 stuff
 import boto3
 from botocore import UNSIGNED
 from botocore.config import Config
-s3=boto3.resource('s3',config=Config(signature_version=UNSIGNED))
 
 
+client = Client("SCEDC")
+s3 = boto3.resource('s3', config=Config(signature_version=UNSIGNED))
+
+
+# where the data is
 BUCKET_NAME = 'scedc-pds'
 
-import obspy
-from obspy.clients.fdsn import Client
-client = Client("SCEDC")
+# where to put the daata
+os.makedirs('./data' , exist_ok=True)
 
 
 net = "CI"
 sta = "RIO"
 chan = "HHZ"
-loc ="*"
-day = "2022-01-01"
-t0 = obspy.UTCDateTime(day)
+loc = "*"
+year = 2022
+mt = 5
+day = 9
+t0 = datetime.datetime( year , mt , day)
+doy = int(t0.strtime("%j"))  # calculate the day of year
+fmin = 0.1  # mininmum frequenc of the bandpass
+fmax = 10   # maximum frequency band
 
-
-KEY='continuous_waveforms/2017/2017_180/'+net+sta+'__'+chan+'___2017180.ms'
-
+tt0 = obspy.UTCDateTime( year , mt ,day)
 fmin = 0.1  # m
 fmax = 10   # maximum frequency band
 
 
-s3.Bucket(BUCKET_NAME).download_file(KEY,net+sta+'__'+chan+'___2017180.ms')
+# get data from seb services
+t0 = time()
+st1=client.get_waveforms(network = net ,station=sta ,location=loc ,channel=chan ,\
+                       starttime=tt0 ,endtime=t0+86400)
+t1=time()
+print("Download time webservices %f", (t1-t0))
+
+# define the file name
+file = net+sta+'__'+chan+'___'+str(year)+str(doy).zfill(3)+'.ms'
+# Key defined by SCEDC bucket
+KEY = 'continuous_waveforms/2017/2017_180/'+file
+
+
+# download data
+t0 = time()
+s3.Bucket(BUCKET_NAME).download_file(KEY, './data/' + file)
+t1 = time()
+print("Download time %f", (t1-t0))
+
+# Compare with webservices
 
 
 
-# Now we extract 2 kinds of feature: 
-#1. Peak amplitude at a specific frequency band
-#2. Peak frequency of the Fourier Amplitude Spectrum
-#We save the data into a CSV file that we will download.
-
-st.filter('bandpass',freqmin=fmin,freqmax=fmax)
-plt.plot(st[0].times(),st[0].data)
+# Feature selection
+st = obspy.read('./data/' + file)
+st.filter('bandpass', freqmin = fmin, freqmax = fmax)
+plt.plot(st[0].times(), st[0].data)
 plt.savefig('test.png')
 
 # get the maximum value in that frequency band
-Amax=np.max(np.abs(st[0].data))
+Amax = np.max(np.abs(st[0].data))
 imax = np.argmax(np.abs(st[0].data))
-Amaxt = st[0].times(type='timestamp')[imax] # this is timestamps since POSIX time (197-,1,1).
-print(Amax,Amaxt)
+Amaxt = st[0].times(type='timestamp')[imax]  # this is timestamps since POSIX time (197-,1,1).
+print( Amax , Amaxt)
 
 
 
 # 2. Fourier Transform
 npts = st[0].stats.npts
-## FFT the signals
+# FFT the signals
 # fill up until 2^N value to speed up the FFT
 Nfft = next_fast_len(int(st[0].data.shape[0])) # this will be an even number
 freqVec = fftfreq(Nfft, d=st[0].stats.delta)[:Nfft//2]
 st.taper(max_percentage=0.05)
-Zhat = fft(st[0].data,n=Nfft)
+Zhat = fft(st[0].data , n=Nfft)
 
 # fig,ax=plt.subplots(1,1,figsize=(11,8))
 # ax.plot(freqVec,np.abs(Zhat[:Nfft//2])/Nfft)
